33aba4f031e6b69f52d84660906fc2ef9f2e7695
Rohan <60407482+Rohan-yadav@users.noreply.github.com>
2020-11-22 11:16:45 -0500

    understanding how to declare variable & print it

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name=rohan
+echo "Myname is $name"
+
+out=$(ls)
+echo "ls command is used to list file name(s) in perticular directory"
+printf "Result of of ls command \n ${out}\n"
+
+#end of script
